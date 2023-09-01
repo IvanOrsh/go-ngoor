104e96498ed8ace492f16dd4942897fe96a4f27c
IvanOrsh <ksironov@mail.ru>
2023-09-01 14:10:21 +0300

    refactor: use templates

--- main.go
+++ main.go
@@ -2,43 +2,52 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
+	"path/filepath"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
-const faqHtml = `
-<h1>FAQ Page</h1>
-<p><b>Q</b>: Is there a free version?</p>
-<p><b>A</b>: Yes! We offer a free trial for 30 days on any paid plans.</p>
-<br />
-<p><b>Q</b>: What are your support hours?</p>
-<p><b>A</b>: We have support staff answering email 24/7, though response times may be a bit slower on weekdays.</p>
-<br />
-<p><b>Q</b>: How do I contact support?</p>
-<p><b>A</b>: Email us - support@lenslocked.com.</p>
-`
+func executeTemplate(w http.ResponseWriter, filepath string) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8") // redundant
+	tpl, err := template.ParseFiles(filepath)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError) // redundant
+		log.Printf("parsing template: %v", err)
+		http.Error(w, "There was an error parsing the template.", http.StatusInternalServerError)
+		return
+	}
+	err = tpl.Execute(w, nil) // once we start writing to ResponseWriter, its' status code is set to 200
+	if err != nil {
+		log.Printf("executing template: %v", err)
+		http.Error(w, "There was an error executing the template.", http.StatusInternalServerError)
+		return
+	}
+}
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8") // redundant
-	w.WriteHeader(http.StatusOK) // redundant
-	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+	tplPath := filepath.Join("templates", "home.gohtml")
+	executeTemplate(w, tplPath)	
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:john@example.com\">john</a></p>")
+	tplPath := filepath.Join("templates", "contact.gohtml")
+	executeTemplate(w, tplPath)
 }
 
 func faqHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprint(w, faqHtml)
+	tplPath := filepath.Join("templates", "faq.gohtml")
+	executeTemplate(w, tplPath)
 }
 
 func main() {
 	r := chi.NewRouter()
+
+	// middlewares
+	r.Use(middleware.Logger)
 
 	r.Get("/", homeHandler)
 	r.Get("/contact", contactHandler)
